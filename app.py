eea46a2616fedb3bdee7c958fe51ce3dc50ed04f
Simranjakhodia <simranjakhodia22986@gmail.com>
2021-07-15 14:48:14 +0530

    Add Files

---
+++ app.py
@@ -0,0 +1,34 @@
+import flask
+import numpy as np
+import sklearn
+import joblib
+from flask import request, render_template
+
+app = flask.Flask(__name__, template_folder='templates' ,static_folder='static' )
+
+from flask_cors import CORS  #for hosting
+CORS(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
+
+@app.route('/predict' , methods=['POST'])
+def predict():
+    
+    model = joblib.load('covid19_prediction_model.ml')
+   
+    a = [float(x) for x in request.form.values()]
+    arr = [np.array(a)]
+   # arr = np.array([a])
+    output_predict = model.predict(arr)
+    return render_template('index.html',prediction_text='The Severity Condition based on the Covid-19 Symptoms: {}'.format(output_predict))
+
+if __name__ == "__main__" :
+    app.run(debug=True)
+
+
+#Run this file by command py app.py after going into the project directory
+# http://127.0.0.1:5000/ this port will get activated showing our REST-API has been created using Flask
